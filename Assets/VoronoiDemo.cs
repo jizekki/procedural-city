8eb43c93c272a5782ca921c1d373a32763445a07
Jalal Izekki <jalal.izekki99@gmail.com>
2022-01-11 15:00:06 +0100

    added initial version of navigation

--- Assets/VoronoiDemo.cs
+++ Assets/VoronoiDemo.cs
@@ -2,23 +2,29 @@ using UnityEngine;
 using System.Collections.Generic;
 using Delaunay;
 using Delaunay.Geo;
+using UnityEngine.AI;
 
 public class VoronoiDemo : MonoBehaviour
 {
 
     public Material land;
-    public Texture2D tx;
     public const int NPOINTS = 100;
     public const int WIDTH = 200;
     public const int HEIGHT = 200;
 	public GameObject road;
+	public UnityEngine.AI.NavMeshAgent car;
 
-    private List<Vector2> m_points;
+	private Texture2D tx;
+	private List<Vector2> m_points;
 	private List<LineSegment> m_edges = null;
 	private List<LineSegment> m_spanningTree;
 	private List<LineSegment> m_delaunayTriangulation;
+	private List<GameObject> roads;
+	private List<UnityEngine.AI.NavMeshAgent> cars;
 
-    private float [,] createMap() 
+
+
+	private float [,] createMap() 
     {
         float [,] map = new float[WIDTH, HEIGHT];
         for (int i = 0; i < WIDTH; i++)
@@ -29,12 +35,13 @@ public class VoronoiDemo : MonoBehaviour
 
 	void Start ()
 	{
-        float [,] map=createMap();
+        float [,] map = createMap();
         Color[] pixels = createPixelMap(map);
 		Debug.Log(pixels[0]);
         /* Create random points points */
 		m_points = new List<Vector2> ();
 		List<uint> colors = new List<uint> ();
+		
 		/* Randomly pick vertices */
 		for (int i = 0; i < NPOINTS; i++) {
 			colors.Add ((uint)0);
@@ -51,58 +58,60 @@ public class VoronoiDemo : MonoBehaviour
 		m_delaunayTriangulation = v.DelaunayTriangulation ();
 
 		Color color = Color.blue;
+		Transform tr = GetComponent<Transform>();
+		roads = new List<GameObject>();
 		/* Shows Voronoi diagram */
 		for (int i = 0; i < m_edges.Count; i++) {
 			LineSegment seg = m_edges [i];				
 			Vector2 left = (Vector2)seg.p0;
 			Vector3 leftScaled = new Vector3(left.y * 10f / WIDTH - 5f, 0.0f, left.x * 10f / HEIGHT - 5f);
-			GameObject r = GameObject.Instantiate(road, leftScaled, Quaternion.identity);
+			GameObject r = GameObject.Instantiate(road, leftScaled, Quaternion.identity, tr);
 			Vector2 right = (Vector2)seg.p1;
 			Vector3 rightScaled = new Vector3(right.y * 10f / WIDTH - 5f, r.transform.position.y, right.x * 10f / HEIGHT - 5f);
 			float size = Vector2.Distance(new Vector2(left.x * 10f / WIDTH, left.y * 10f / HEIGHT), new Vector2(right.x * 10f / WIDTH, right.y * 10f / HEIGHT));
 			r.transform.LookAt(rightScaled);
 			r.transform.localScale = new Vector3(r.transform.localScale.x, r.transform.localScale.y, size);
 			r.transform.position = (leftScaled + rightScaled) / 2;
-
-			
-			DrawLine (pixels, left, right, color);
+			roads.Add(r);
 		}
 
 
 		color = Color.red;
-		/* Shows Delaunay triangulation */
-		/*if (m_delaunayTriangulation != null) {
-			for (int i = 0; i < m_delaunayTriangulation.Count; i++) {
-					LineSegment seg = m_delaunayTriangulation [i];				
-					Vector2 left = (Vector2)seg.p0;
-					Vector2 right = (Vector2)seg.p1;
-					DrawLine (pixels,left, right,color);
-			}
-		}*/
 
-        /* Shows spanning tree */
-        /*
-		color = Color.black;
-		if (m_spanningTree != null) {
-			for (int i = 0; i< m_spanningTree.Count; i++) {
-				LineSegment seg = m_spanningTree [i];				
-				Vector2 left = (Vector2)seg.p0;
-				Vector2 right = (Vector2)seg.p1;
-				DrawLine (pixels,left, right,color);
-			}
-		}*/
         /* Apply pixels to texture */
         tx = new Texture2D(WIDTH, HEIGHT);
         land.SetTexture ("_MainTex", tx);
 		tx.SetPixels (pixels);
 		tx.Apply ();
 
+		cars = new List<UnityEngine.AI.NavMeshAgent>();
+
+		NavMeshSurface surface = GetComponent<NavMeshSurface>();
+		surface.BuildNavMesh();
+
+		UnityEngine.AI.NavMeshAgent a = Instantiate(car, roads[Random.Range(0, roads.Count)].transform.position, Quaternion.identity, tr);
+		cars.Add(a);
+		cars[0].destination = new Vector3(Random.Range(3f, 5f), 0f, Random.Range(3f, 5f));
+
 	}
 
 
 
-    /* Functions to create and draw on a pixel array */
-    private Color[] createPixelMap(float[,] map)
+	void Update()
+    {
+		foreach(UnityEngine.AI.NavMeshAgent car in cars)
+        {
+			if(car.remainingDistance == 0)
+            {
+				car.destination = new Vector3(Random.Range(-5f, 5f), 0f, Random.Range(-5f, 5f));
+			}
+        }
+	}
+
+
+
+	/* Functions to create and draw on a pixel array */
+	private Color[] createPixelMap(float[,] map)
     {
         Color[] pixels = new Color[WIDTH * HEIGHT];
         for (int i = 0; i < WIDTH; i++)
@@ -112,36 +121,4 @@ public class VoronoiDemo : MonoBehaviour
             }
         return pixels;
     }
-    private void DrawPoint (Color [] pixels, Vector2 p, Color c) {
-		if (p.x<WIDTH&&p.x>=0&&p.y<HEIGHT&&p.y>=0) 
-		    pixels[(int)p.x*HEIGHT+(int)p.y]=c;
-	}
-	// Bresenham line algorithm
-	private void DrawLine(Color [] pixels, Vector2 p0, Vector2 p1, Color c) {
-		int x0 = (int)p0.x;
-		int y0 = (int)p0.y;
-		int x1 = (int)p1.x;
-		int y1 = (int)p1.y;
-
-		int dx = Mathf.Abs(x1-x0);
-		int dy = Mathf.Abs(y1-y0);
-		int sx = x0 < x1 ? 1 : -1;
-		int sy = y0 < y1 ? 1 : -1;
-		int err = dx-dy;
-		while (true) {
-            if (x0>=0&&x0<WIDTH&&y0>=0&&y0<HEIGHT)
-    			pixels[x0*HEIGHT+y0]=c;
-
-			if (x0 == x1 && y0 == y1) break;
-			int e2 = 2*err;
-			if (e2 > -dy) {
-				err -= dy;
-				x0 += sx;
-			}
-			if (e2 < dx) {
-				err += dx;
-				y0 += sy;
-			}
-		}
-	}
 }
